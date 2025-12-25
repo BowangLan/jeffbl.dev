0c1c2679d1570840abcca68d5c112915b177c5fa
Jeffrey Lan <blan2@uw.edu>
2025-12-24 21:35:43 -0800

    fix: make 'active' prop optional in DefaultLayout and ensure HomeNavigation handles undefined values gracefully

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -10,11 +10,11 @@ export default function DefaultLayout({
   active,
 }: {
   children: React.ReactNode;
-  active: string;
+  active?: string;
 }) {
   return (
     <div className="z-10 flex flex-col items-stretch flex-1 w-full px-4 mx-auto lg:max-w-4xl md:px-8 xl:max-w-6xl min-h-[calc(100vh-228px)]">
-      <HomeNavigation active={active} />
+      <HomeNavigation active={active ?? ""} />
 
       <AnimatePresence>
         <motion.main
